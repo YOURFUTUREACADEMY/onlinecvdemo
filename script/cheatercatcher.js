2062ed2bc98a549a94a03f69dce6db8c61d5134e
Dalstam <danielvdstam@gmail.com>
2021-02-25 14:50:54 +0100

    No inspector

--- script/cheatercatcher.js
+++ script/cheatercatcher.js
@@ -1,50 +1,36 @@
 
-var element = new Image;
-var devtoolsOpen = false;
 
-element.__defineGetter__("id", function () {
-    devtoolsOpen = true; // This only executes when devtools is open.
+//Als er gedrukt wordt op een van deze sneltoetsen word je gelijk doorgestuurd naar een volgende pagina.
+//keydown betekent dat er iets moet gebeuren als er op een knop gedrukt word.
+//In dit geval doorsturen naar de blue screen of death pagina.
+//De e in de function(e) staat voor het indrukken van een knop.
+document.addEventListener("keydown", function(e){
+    e.preventDefault();
+  //Als er gedrukt wordt op ctrl shift i word je naar de blue screen of death pagina gestuurd.
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+    window.location.href = "bluescreen.html";
+}
+//Als er gedrukt wordt op ctrl shift j word je naar de blue screen of death pagina gestuurd.
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+  window.location.href = "bluescreen.html";
+}
+//Als er gedrukt wordt op ctrl shift u word je naar de blue screen of death pagina gestuurd.
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'U'.charCodeAt(0)){
+  window.location.href = "bluescreen.html";
+}
+//Als er gedrukt wordt op alt f word je naar de blue screen of death pagina gestuurd.
+if(e.altKey && e.keyCode == 'F'.charCodeAt(0)){ 
+  window.location.href = "bluescreen.html";
+}
+if(e.keycode == 'devtools'){
+  window.location.href = "bluescreen.html";
+}
 });
-
-setInterval(function () {
-
-
-       document.onkeydown = function (e) {
-        if (e.keyCode == 123) {
-            return false;
-        }
-
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-            window.location.href = "bluescreen.html";
-            return false;
-
-        }
-
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-            window.location.href = "bluescreen.html";
-            return false;
-        }
-
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 'U'.charCodeAt(0)) {
-            window.location.href = "bluescreen.html";
-            return false;
-        }
-
-        if (e.altKey && e.keyCode == 'F'.charCodeAt(0)) {
-            window.location.href = "bluescreen.html";
-            return false;
-        }
-    }
-    document.addEventListener("contextmenu", function (beveiliging) {
-        beveiliging.preventDefault();
-    });
-
-    devtoolsOpen = false;
-    console.log(element);
-    if (devtoolsOpen == true) {
-        window.location.href = "bluescreen.html";
-    }
-}, 1000);
+//Als er op de rechtermuisknop word gedrukt gelijk naar blue screen of death pagina
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+   window.location.href = "bluescreen.html";
+});
 
 
 
---
+++ script/restart.js
@@ -0,0 +1,19 @@
+document.querySelector("#restart").addEventListener('click', ()=>{
+    // window.close;
+    window.open('index.html',  '_blank')
+    window.close();
+});
+
+
+window.location.hash = "CRAASHHHHHHHHH";
+
+// Again because Google Chrome doesn't insert
+// the first hash into the history
+window.location.hash = "CRAASHHHHHHHHH"; 
+
+window.onhashchange = function(){
+    window.location.hash = "CRAASHHHHHHHHH";
+}
+
+
+  
