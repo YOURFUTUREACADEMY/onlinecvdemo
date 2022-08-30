1dd4ad1872c35afa6ea8c1a64f504ab700f80476
Dalstam <daniel@yourfutureacademy.nl>
2022-08-30 12:38:15 +0200

    new version with bootstrap and bluescreenjs

---
+++ js/bluescreen.js
@@ -0,0 +1,30 @@
+var element = new Image;
+var devtoolsOpen = false;
+element.__defineGetter__("id", function () {
+  devtoolsOpen = true; // This only executes when devtools is open.
+});
+
+//keydown betekent dat er iets moet gebeuren als er op een knop gedrukt word.
+//In dit geval doorsturen naar de blue screen of death pagina.
+//De event in de function(eevent) staat voor het indrukken van een knop.
+//Het indrukken van een knop (behalve ArrowUp en ArrowDown) zorgt ervoor dat je naar de bluescreen wordt gestuurd.
+
+document.addEventListener('keydown', function(event){
+  if(event.code != 'ArrowUp' || event.code != 'ArrowDown'){
+    window.location.href = "bluescreen.html";
+  }
+})
+
+//Als er op de rechtermuisknop word gedrukt gelijk naar blue screen of death pagina
+document.addEventListener("contextmenu", function (e) {
+  window.location.href = "bluescreen.html";
+});
+
+window.addEventListener('devtoolschange', event => {
+  console.log('Is DevTools open:', event.detail.isOpen);
+  console.log('DevTools orientation:', event.detail.orientation);
+});
+
+
+
+
